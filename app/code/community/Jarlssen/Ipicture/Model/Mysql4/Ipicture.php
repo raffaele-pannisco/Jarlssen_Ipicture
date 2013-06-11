3d4e4a73d90b8f2f2625a407406665efae9f034b
Raffaele Pannisco <raffaele.pannisco@jarlssen.de>
2013-06-11 15:58:49 +0200

    adding comments

--- app/code/community/Jarlssen/Ipicture/Model/Mysql4/Ipicture.php
+++ app/code/community/Jarlssen/Ipicture/Model/Mysql4/Ipicture.php
@@ -6,6 +6,7 @@ class Jarlssen_Ipicture_Model_Mysql4_Ipicture extends Mage_Core_Model_Mysql4_Abs
     {    
         // Note that the ipicture_id refers to the key field in your database table.
         $this->_init('ipicture/ipicture', 'ipicture_id');
+        //our primary key is not incremental, we must tell this to magento
         $this->_isPkAutoIncrement = false;
     }
 }
